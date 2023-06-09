2505ca167a4d04ac6a5aa8185c8b9918133ac76e
Kun <1433729173@qq.com>
2023-06-09 21:38:39 +0800

    comport file

---
+++ NB-Iot/comport.c
@@ -0,0 +1,21 @@
+/*********************************************************************************
+ *      Copyright:  (C) 2023 Kun_<1433729173@qq.com>
+ *                  All rights reserved.
+ *
+ *       Filename:  comport.c
+ *    Description:  This file 
+ *                 
+ *        Version:  1.0.0(2023年06月09日)
+ *         Author:  Kun_ <1433729173@qq.com>
+ *      ChangeLog:  1, Release initial version on "2023年06月09日 21时36分09秒"
+ *                 
+ ********************************************************************************/
+
+int comport_open(comport_t *comport, char *devname, long baudrate, char *conf);
+
+int comport_close(comport_t *comport);
+
+int comport_send(comport_t *comport, char *data, int bytes);
+
+int comport_recv(comport_t *comport, char *buf, int size, int timeout);
+
